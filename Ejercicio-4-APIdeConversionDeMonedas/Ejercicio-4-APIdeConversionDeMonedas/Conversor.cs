8d0a210c842389e6a257a7910c7bc59ee8cd0068
Cristian Peluka <cristiancamposorue@gmail.com>
2025-03-04 18:53:45 -0500

    Agregando el ejercicio 4 a la carpeta de ejercicios

---
+++ Ejercicio-4-APIdeConversionDeMonedas/Ejercicio-4-APIdeConversionDeMonedas/Conversor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4_APIdeConversionDeMonedas
+{
+    internal class Conversor
+    {
+        // metodo para convertir DOLARES a SOLES
+        public static decimal ConversorMoneda(double CantidadDolares)
+        {
+            decimal tipoCambio = 3.67M; // el tipo de cambio 1 dolar equivale a 3.67 soles
+
+            return (decimal)CantidadDolares * tipoCambio;
+        }
+
+        //metodo para convertir DOLARES a EURO
+        public static decimal ConversorMoneda(decimal CantidadDolares)
+        {
+            decimal tipoCambio = 0.94M; // el tipo de cambio 1 dolar equivale a 0.94 euros
+            return CantidadDolares * tipoCambio;
+        }
+
+        // metodo para convertir CUALQUIER MONEDA a OTRA
+        public static decimal ConversorMoneda(decimal CantidadMoneda, decimal tipoCambio)
+        {
+            return CantidadMoneda * tipoCambio;
+        }
+    }
+}
---
+++ Ejercicio-4-APIdeConversionDeMonedas/Ejercicio-4-APIdeConversionDeMonedas/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4_APIdeConversionDeMonedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool esverdad = false;
+
+            do
+            {
+                
+                
+                Console.WriteLine("1. Dolares a Soles \n2. Dolares a Euros \n3. Otros \n4. Salir");
+                Console.Write("Elija el tipo de conversion que desea realizar: ");
+                int eleccion = int.Parse(Console.ReadLine());
+
+
+                switch (eleccion)
+                {
+                    case 1:
+                        Console.Write("Ingrese la cantidad de dolares a convertir: ");
+                        double dolares = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"{dolares} dolares equivale a {Conversor.ConversorMoneda(dolares)} soles");
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.Write("Ingrese la cantidad de dolares a convertir: ");
+                        decimal Dolares = decimal.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"{Dolares} dolares equivale a {Conversor.ConversorMoneda(Dolares)} euros");
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Indique el nombre de la moneda que quiere convertir: ");
+                        string moneda = Console.ReadLine();
+
+                        Console.WriteLine("Ingrese el nombre de la moneda a convertir: ");
+                        string Otramoneda = Console.ReadLine();
+
+                        Console.WriteLine($"Ingrese la cantidad de {moneda} a convertir");
+                        decimal CantidadMoneda = decimal.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Ahora indique el tipo de cambio de la conversion: ");
+                        decimal tipoCambio = decimal.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"{CantidadMoneda} {moneda} equivale a {Conversor.ConversorMoneda(CantidadMoneda, tipoCambio)} {Otramoneda}");
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo del programa.....");
+                        break;
+                    default:
+                        Console.WriteLine("opcion invalida");
+                        Console.WriteLine("Desea realizar una conversion?");
+                        string respuesta = Console.ReadLine().ToLower();
+
+                        if (respuesta == "si")
+                        {
+                            esverdad = false;
+                            break;
+                        }
+                        else if (respuesta == "no")
+                        {
+                            esverdad= true;
+                            Console.WriteLine("Cerrando programa....");
+                            break;
+                        }
+                        break;
+                        
+                }
+            }
+            while (!esverdad);
+
+        }
+    }
+}
